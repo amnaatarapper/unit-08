76a2d52c78b7d902355692b73b39f2e0995a5bfa
Oussama <oussama.aitbaziz@gmail.com>
2019-08-08 09:19:03 +0100

    '/' redirect, '/books', '/books/new working '

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
+const express = require('express');
+const app = express();
+
+
+// Sequelize
 const db = require('./db');
 const { Book } = db.models;
 
-(async () => {
-  await db.sequelize.sync();
+// db connection test
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+// static assets
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+
+// (async () => {
+  
+//   await db.sequelize.sync();
+
+//   try {
+//     const books = await Book.findAll({
+
+//     });
+//     console.log( books.map(book => book.toJSON()) );
+
+//   } catch(error) {
+//     if (error.name === 'SequelizeValidationError') {
+//       const errors = error.errors.map(err => err.message);
+//       console.error('Validation errors: ', errors);
+//     } else {
+//       throw error;
+//     }
+//   }
+// })();
+
+app.get('/', (req, res) => {
+  res.redirect('/books');
+});
+
+app.get('/books', async (req, res) => {
+
+  let booksData = [];
+  
 
   try {
-    const books = await Book.findAll({
-      
+    const books = await Book.findAll({})
+    books.map( book => {
+      booksData.push(book.toJSON())
     });
-    console.log( books.map(book => book.toJSON()) );
+    console.log( books.map(book => book.toJSON()));
+
 
   } catch(error) {
     if (error.name === 'SequelizeValidationError') {
@@ -18,4 +68,30 @@ const { Book } = db.models;
       throw error;
     }
   }
-})();
+
+  res.render('all_books', { booksData });
+});
+
+
+app.get('/books/new', (req, res) => {
+  res.render('new_book', {});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+db.sequelize.sync().then(() => {
+  app.listen(3000, () => console.log("Running on port 3000"));
+});
+
+
+
+

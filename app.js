35b29326b5cc23f084a183e71251c69545056fad
Oussama <oussama.aitbaziz@gmail.com>
2019-08-10 09:02:19 +0100

    review ready

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
-// Sequelize
+// SEQUELIZE
 const db = require('./db');
-const { Book } = db.models;
+const {
+  Book
+} = db.models;
 
-// db connection test
+// DB CONNECTION TEST
 db.sequelize
   .authenticate()
   .then(() => {
@@ -18,12 +20,16 @@ db.sequelize
   });
 
 
-// static assets
+// SERVE STATIC ASSETS
 app.use('/static', express.static('public'));
 
+// SET VIEW ENGINE
 app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded( { extended: false }));
+// PARSE HTTP RES/REQ
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 
@@ -41,15 +47,17 @@ app.get('/books', async (req, res) => {
 
     let booksData = [];
     const books = await Book.findAll({})
-    books.map( book => {
+    books.map(book => {
       booksData.push(book.toJSON())
     });
     // console.log( books.map(book => book.toJSON()));
-    res.render('all_books', { booksData });
+    res.render('all_books', {
+      booksData
+    });
 
-  } catch(error) {
-      res.render('error');
-    }
+  } catch (error) {
+    res.render('error');
+  }
 
 });
 
@@ -60,20 +68,20 @@ app.get('/books/new', (req, res) => {
 
 // CREATE NEW BOOK REQUIRE VALIDATION
 app.post('/books/new', async (req, res, next) => {
-  
+
   try {
 
     const book = await Book.create(req.body);
     res.redirect('/books/' + book.id);
 
-  } catch(error) {
+  } catch (error) {
     if (error.name === 'SequelizeValidationError') {
       const errors = error.errors.map(err => err.message);
       console.error('Validation errors: ', errors);
       next();
     } else {
       res.render('error');
-      }
+    }
   }
 });
 
@@ -85,7 +93,6 @@ app.get('/books/:id', async (req, res) => {
     let targetedBook;
     let bookData = {};
 
-
     const book = await Book.findByPk(bookId);
     targetedBook = book.toJSON();
     bookData = {
@@ -95,12 +102,14 @@ app.get('/books/:id', async (req, res) => {
       genre: targetedBook.genre,
       year: targetedBook.year,
     };
-    
-    res.render('book_detail', { bookData });
 
-  } catch(error) {
-      res.render('error');
-    }
+    res.render('book_detail', {
+      bookData
+    });
+
+  } catch (error) {
+    res.render('error');
+  }
 });
 
 // UPDATE BOOK DATA REQUIRE VALIDATION
@@ -108,20 +117,19 @@ app.post('/books/:id', async (req, res) => {
 
   try {
     const bookId = req.params.id;
-
     const book = await Book.findByPk(bookId);
     await book.update(req.body);
 
     res.redirect(`/books/${book.id}`);
 
-  } catch(error) {
+  } catch (error) {
     if (error.name === 'SequelizeValidationError') {
       const errors = error.errors.map(err => err.message);
       console.error('Validation errors: ', errors);
       next();
     } else {
       res.render('error');
-      }
+    }
   }
 });
 
@@ -135,9 +143,9 @@ app.post('/books/:id/delete', async (req, res) => {
 
     res.redirect(`/books`);
 
-  } catch(error) {
-      res.render('error');
-    }
+  } catch (error) {
+    res.render('error');
+  }
 });
 
 // 404 Route
@@ -146,12 +154,16 @@ app.use((req, res, next) => {
   error.status = 404;
   next(error);
 });
+
 app.use((err, req, res, next) => {
-  
 
+  // ERROR DETAILS TO THE USER
   res.status(err.status);
-  res.render('404', { error: err })
+  res.render('404', {
+    error: err
+  })
 
+  // ERROR DETAILS ON THE CONSOLE
   let errorMessage = "Request message: " + err.message;
   errorMessage += "\nRequest status: " + err.status;
   errorMessage += "\nError stack: " + err.stack;
@@ -160,13 +172,11 @@ app.use((err, req, res, next) => {
 });
 
 
+// SYNC THE DATABASE AND SERVE THE APP
 db.sequelize.sync().then(() => {
 
   app.listen(process.env.PORT || 3000, () => {
     console.log('server is up on localhost:3000')
-})
+  })
 
 });
-
-
-
